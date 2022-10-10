e07c1c9c0f2fd9bc68b92e9d340ca18246c76f13
Ahmed Khan <106860847+Wenster007@users.noreply.github.com>
2022-10-10 07:41:53 +0500

    2nd commit
    Added sound effects

--- index.js
+++ index.js
@@ -1,105 +1,114 @@
 "strict mode";
 
 
-document.addEventListener("keydown", function(e){
-    console.log(e);
+//Function for playing audio
+function play1() {
+    let audio1 = document.getElementById("audio1");
+    audio1.pause();
+    audio1.currentTime = 0;
+    audio1.play(); 
+}
 
-    if (e.key === "`"){
+document.addEventListener("keydown", function (e) {
+    console.log(e);
+    
+    play1();
+    if (e.key === "`") {
         document.getElementById("backtick").style.backgroundColor = "green";
-    } else if(e.key === "1"){
+    } else if (e.key === "1") {
         document.getElementById("one").style.backgroundColor = "green";
-    }else if(e.key === "2"){
+    } else if (e.key === "2") {
         document.getElementById("two").style.backgroundColor = "green";
-    }else if(e.key === "3"){
+    } else if (e.key === "3") {
         document.getElementById("three").style.backgroundColor = "green";
-    }else if(e.key === "4"){
+    } else if (e.key === "4") {
         document.getElementById("four").style.backgroundColor = "green";
-    }else if(e.key === "5"){
+    } else if (e.key === "5") {
         document.getElementById("five").style.backgroundColor = "green";
-    }else if(e.key === "6"){
+    } else if (e.key === "6") {
         document.getElementById("six").style.backgroundColor = "green";
-    }else if(e.key === "7"){
+    } else if (e.key === "7") {
         document.getElementById("seven").style.backgroundColor = "green";
-    }else if(e.key === "8"){
+    } else if (e.key === "8") {
         document.getElementById("eight").style.backgroundColor = "green";
-    }else if(e.key === "9"){
+    } else if (e.key === "9") {
         document.getElementById("nine").style.backgroundColor = "green";
-    }else if(e.key === "0"){
+    } else if (e.key === "0") {
         document.getElementById("zero").style.backgroundColor = "green";
-    } else if(e.key === "-"){
+    } else if (e.key === "-") {
         document.getElementById("minus").style.backgroundColor = "green";
-    }else if(e.key === "="){
+    } else if (e.key === "=") {
         document.getElementById("equal").style.backgroundColor = "green";
-    }else if(e.key === "["){
+    } else if (e.key === "[") {
         document.getElementById("squareOpen").style.backgroundColor = "green";
-    }else if(e.key === "]"){
+    } else if (e.key === "]") {
         document.getElementById("squareClose").style.backgroundColor = "green";
-    }else if(e.key === "\\"){
+    } else if (e.key === "\\") {
         document.getElementById("backSlash").style.backgroundColor = "green";
-    }else if(e.key === ";"){
+    } else if (e.key === ";") {
         document.getElementById("colun").style.backgroundColor = "green";
-    }else if(e.key === "'"){
+    } else if (e.key === "'") {
         document.getElementById("quotation").style.backgroundColor = "green";
-    }else if(e.key === "."){
+    } else if (e.key === ".") {
         document.getElementById("greaterThan").style.backgroundColor = "green";
-    }else if(e.key === ","){
+    } else if (e.key === ",") {
         document.getElementById("lessThan").style.backgroundColor = "green";
-    }else if(e.key === "/"){
+    } else if (e.key === "/") {
         document.getElementById("questionMark").style.backgroundColor = "green";
-    }else if(e.code === "Space"){
+    } else if (e.code === "Space") {
         document.getElementById("Space").style.backgroundColor = "green";
-    } else{
+    } else {
         document.querySelector(`#${e.key}`).style.backgroundColor = "green";
     }
 })
 
-document.addEventListener("keyup", function(e){
+document.addEventListener("keyup", function (e) {
 
-    if (e.key === "`"){
+    if (e.key === "`") {
         document.getElementById("backtick").style.backgroundColor = "white";
-    } else if(e.key === "1"){
+    } else if (e.key === "1") {
         document.getElementById("one").style.backgroundColor = "white";
-    }else if(e.key === "2"){
+    } else if (e.key === "2") {
         document.getElementById("two").style.backgroundColor = "white";
-    }else if(e.key === "3"){
+    } else if (e.key === "3") {
         document.getElementById("three").style.backgroundColor = "white";
-    }else if(e.key === "4"){
+    } else if (e.key === "4") {
         document.getElementById("four").style.backgroundColor = "white";
-    }else if(e.key === "5"){
+    } else if (e.key === "5") {
         document.getElementById("five").style.backgroundColor = "white";
-    }else if(e.key === "6"){
+    } else if (e.key === "6") {
         document.getElementById("six").style.backgroundColor = "white";
-    }else if(e.key === "7"){
+    } else if (e.key === "7") {
         document.getElementById("seven").style.backgroundColor = "white";
-    }else if(e.key === "8"){
+    } else if (e.key === "8") {
         document.getElementById("eight").style.backgroundColor = "white";
-    }else if(e.key === "9"){
+    } else if (e.key === "9") {
         document.getElementById("nine").style.backgroundColor = "white";
-    }else if(e.key === "0"){
+    } else if (e.key === "0") {
         document.getElementById("zero").style.backgroundColor = "white";
-    } else if(e.key === "-"){
+    } else if (e.key === "-") {
         document.getElementById("minus").style.backgroundColor = "white";
-    }else if(e.key === "="){
+    } else if (e.key === "=") {
         document.getElementById("equal").style.backgroundColor = "white";
-    }else if(e.key === "["){
+    } else if (e.key === "[") {
         document.getElementById("squareOpen").style.backgroundColor = "white";
-    }else if(e.key === "]"){
+    } else if (e.key === "]") {
         document.getElementById("squareClose").style.backgroundColor = "white";
-    }else if(e.key === "\\"){
+    } else if (e.key === "\\") {
         document.getElementById("backSlash").style.backgroundColor = "white";
-    }else if(e.key === ";"){
+    } else if (e.key === ";") {
         document.getElementById("colun").style.backgroundColor = "white";
-    }else if(e.key === "'"){
+    } else if (e.key === "'") {
         document.getElementById("quotation").style.backgroundColor = "white";
-    }else if(e.key === "."){
+    } else if (e.key === ".") {
         document.getElementById("greaterThan").style.backgroundColor = "white";
-    }else if(e.key === ","){
+    } else if (e.key === ",") {
         document.getElementById("lessThan").style.backgroundColor = "white";
-    }else if(e.key === "/"){
+    } else if (e.key === "/") {
         document.getElementById("questionMark").style.backgroundColor = "white";
-    }else if(e.code === "Space"){
+    } else if (e.code === "Space") {
         document.getElementById("Space").style.backgroundColor = "white";
-    } else{
+    } else {
         document.querySelector(`#${e.key}`).style.backgroundColor = "white";
     }
 })
